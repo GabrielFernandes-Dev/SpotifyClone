d6aac81986b96a2f69d64efde1b53dc27bacafd7
gabrielgomes-coder <ggfernandes30@gmail.com>
2023-10-26 17:58:36 -0300

    corrigindo capa albuns

--- src/components/Recomendations.vue
+++ src/components/Recomendations.vue
@@ -12,6 +12,11 @@ export default {
     async created() {
         const res = await axios.get('http://localhost:3000/playlists')
         this.playlists = res.data
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL(`${name}`,import.meta.url).href
+        }
     }
 }
 
@@ -22,7 +27,7 @@ export default {
             <div class="block w-44 rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-neutral-700 card-hover">
                 <router-link to="/playlist" @click="playlistSelecionada = playlist.musicas">
                     <img class="rounded-t-lg w-full h-44 object-cover"
-                    :src= "playlist.capa" 
+                    :src= "getImageUrl(playlist.capa)" 
                     alt="Capa da playlist"/>
                 </router-link>
                 <div class="p-4">
