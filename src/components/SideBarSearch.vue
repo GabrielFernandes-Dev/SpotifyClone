962823df0a9ad111ff1ea0e3dd397a2c94c642b9
gabrielgomes-coder <ggfernandes30@gmail.com>
2023-10-31 18:13:13 -0300

    Ajustando css search bar

--- src/components/SideBarSearch.vue
+++ src/components/SideBarSearch.vue
@@ -38,7 +38,7 @@
 </script>
 <template>
     <div class="mb-3">
-        <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+        <div class="relative flex w-full flex-wrap items-stretch">
             <input
             type="search"
             class="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-gray-100 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
@@ -47,7 +47,8 @@
             aria-describedby="button-addon3" 
             v-model="searchValue"
             @click="open = !open"
-            v-on:blur="clearSearch()"/>
+            v-on:blur="clearSearch()"
+            />
 
             <!--Search button-->
             <button
@@ -63,12 +64,13 @@
        </div>
     </div>
 
-    <div class="bg-zinc-900 border-gray-950 lg:border-l-0 lg:border-t lg:border-gray-1000 rounded-b lg:rounded-b-none lg:rounded-r p-2 flex flex-col justify-between leading-normal mx-2 mb-2 w-96" v-show="open"> 
+    <div class="bg-zinc-900 border-gray-950 rounded-md ml-44 p-2 flex flex-col justify-between leading-normal mr-1 mb-2 w-52" v-show="open"> 
         <table class="columns-2 text-gray-100 text-left">
             <tbody v-for="musica in listaMusicas" v-bind:key="musica.idMusica">
                 <tr>
                 <td><button @click="musicaSelecionada = musica">{{ musica.nomeMusica }}</button></td>
-                <td><button
+                <td>
+                    <button
                         class="relative z-[2] rounded-full px-1 py-1 bg-purple-600"
                         type="button"
                         id="button-addon4"
@@ -77,7 +79,8 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
-                    </button></td>
+                    </button>
+                </td>
                 </tr>
             </tbody>
         </table>
