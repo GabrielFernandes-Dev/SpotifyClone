0ce6088b2fc9877b58d4fb61dbc43370c63eb283
ShingenTakeda <gustakeda@gmail.com>
2023-10-31 15:25:20 -0300

    Modal smi pronto

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   import { RouterView } from 'vue-router'
   import NavBar from './components/NavBar.vue'
   import Footer from './components/Footer.vue'
+  import ModalPlaylist from "./components/ModalPlaylist.vue";
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
     <NavBar/>
     <RouterView />
+    <ModalPlaylist>
+      <p>
+        Hi from modal
+      </p>
+    </ModalPlaylist>
     <Footer/>
   </div>
 </template>
---
+++ src/components/ModalPlaylist.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-show="modalActive" class="absolute w-full bg-black bg-opacity-30 h-screen 
+    top-0 left-0 flex justify-content px-8">
+
+        <div v-show="modalActive" class="p4 bg-black self-start mt-32 max-width max-w-screen-md">
+            <slot/>
+
+            <button class="text-white mt-8 bg-purple-500 hover:bg-purple-700">Fechar</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    modalActive: {
+        type:Boolean,
+        default: false
+    }
+})
+</script>
+
+<style></style>
+
