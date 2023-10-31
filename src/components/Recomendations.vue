8dbdc505d8d5dac6061a8551962d67c7a7f213d0
Gabriela Abreu <gabrielaabreu@edu.unifor.br>
2023-10-31 11:56:15 -0300

    Buscar música por nome funcionando

--- src/components/Recomendations.vue
+++ src/components/Recomendations.vue
@@ -15,6 +15,7 @@ export default {
     },
     methods: {
         getImageUrl(name) {
+            
             return new URL(`${name}`,import.meta.url).href
         }
     }
--- src/components/SideBarSearch.vue
+++ src/components/SideBarSearch.vue
@@ -1,4 +1,6 @@
 <script>
+    import axios from 'axios'
+
     export default {
 
         data() {
@@ -9,30 +11,25 @@
         },
         methods: {
             clearSearch() {
-                searchValue = ''
+                this.searchValue = ''
             }
         },
         async created() {
             const resp = await axios.get('http://localhost:3000/musicas')
-            this.musicas = resp.data
+            this.musicas = resp.data.slice(0, 9)
 
         }, 
         computed: {
             listaMusicas() {
-                if(this.searchValue.length > 0){
-                    return this.musicas.filter((musica) => musica.nomeMusica
-                    .includes(this.searchValue))
-                }
-                
-            }
-
-            /*listaMusicas() {
                 if(this.searchValue.trim().length > 0){
                     return this.musicas.filter((musica) => musica.nomeMusica.toLowerCase()
                     .includes(this.searchValue.trim().toLowerCase()))
                 }
                 
-            }*/
+            },
+            adicionarMusica(){
+
+            }
         }
     }
 </script>
@@ -41,7 +38,7 @@
         <div class="relative mb-4 flex w-full flex-wrap items-stretch">
             <input
             type="search"
-            class="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+            class="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-gray-100 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
             placeholder="Search"
             aria-label="Search"
             aria-describedby="button-addon3" 
@@ -61,20 +58,24 @@
        </div>
     </div>
 
-    <div v-for="musica in listaMusicas" v-bind:key="musica.idMusica"> 
-        <table class="table-auto">
-            <tbody>
+    <div class="bg-zinc-900 border-gray-950 lg:border-l-0 lg:border-t lg:border-gray-1000 rounded-b lg:rounded-b-none lg:rounded-r p-2 flex flex-col justify-between leading-normal mx-2 mb-2 w-96"> 
+        <table class="columns-2 text-gray-100 text-left">
+            <tbody v-for="musica in listaMusicas" v-bind:key="musica.idMusica">
                 <tr>
                 <td>{{ musica.nomeMusica }}</td>
-                <td><button>Adicionar</button></td>
+                <td><button
+                        class="relative z-[2] rounded-full px-1 py-1 bg-purple-600"
+                        type="button"
+                        id="button-addon4"
+                        data-te-ripple-init
+                        onclick="adicionarMusica()">
+                        <img src="../images/mais.jpg" alt="Adicionar música" width="10" height="10">
+                    </button></td>
                 </tr>
             </tbody>
         </table>
     </div>
 
-    <div>
-        {{ searchValue }}
-        {{ musicas }}
-    </div>
+
 
 </template>
