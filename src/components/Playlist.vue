0e86ff57bc197eacbbc7482e349af4e24df61dd8
ShingenTakeda <gustakeda@gmail.com>
2023-11-21 15:44:03 -0300

    Got it working, finally

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -22,6 +22,7 @@ export default {
     async created() {
         await this.getPlaylist()
         await this.getMusicas()
+        await this.getAllSongs()
     },
     watch: {
         $route(to, from) {
@@ -67,14 +68,35 @@ export default {
                 await axios.patch('http://localhost:3000/playlists/' + this.playlistId, this.playlistSelecionada)
             }
             catch(error) {
-                console.log("Não pode remover musica")
+                console.log("Não pode remover musica! ERRO:" + error)
+            }
+        },
+        async getAllSongs(){
+            try
+            {
+                const res = await axios.get('http://localhost:3000/musicas')
+                this.musicas = res.data
+                console.log(this.musicas)
+            }
+            catch(error)
+            {
+                console.log("Não pode pegar as musicas! ERRO:" + error)
             }
         },
         estadoModal(){
             this.modalOpen = !this.modalOpen;
         },
-        async addMusica(){
-
+        async addMusica(musicaID){
+            console.log(this.playlistSelecionada)
+            console.log(this.musicasPlaylist)
+            try {
+                this.playlistSelecionada.musicas.push(musicaID);
+                console.log(`adicioanando musica com id: ${musicaID}`)
+                await axios.patch('http://localhost:3000/playlists/' + this.playlistId, this.playlistSelecionada)
+            }
+            catch(error) {
+                console.log("Não pode remover musica! ERRO:" + error)
+            }
         }
     },
 }
@@ -141,28 +163,33 @@ export default {
             <source :src="btn" type="audio/ogg">
         </audio> 
     </div> -->
-</div>
+</div >
 
 <div v-show="modalOpen" class="absolute w-full bg-black bg-opacity-30 h-screen 
-    top-0 left-0 flex justify-content px-8">
+    top-0 left-0 flex justify-content justify-center  px-8  place-items-center">
 
-    <form class="bg-white">
-        <div class="mb-6">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required>
-        </div>
-        <div class="mb-6">
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-        </div>
-        <div class="flex items-start mb-6">
-            <div class="flex items-center h-5">
-            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required>
-            </div>
-            <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-        </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-    </form>
+    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <button @click="estadoModal" type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            X
+        </button>
+
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+
+        <tbody>
+            <tr v-for="n in musicas" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ n.nomeMusica }}
+                </th>
+                <td class="px-6 py-4">
+                    <button @click="addMusica(n.idMusica)" type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                            Adicionar
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    </div>
 </div>
 </template>
 
